9fcd3d92100399dbdde194b3d4fed6c155039b4b
Samantha Castillo <samanthacas@pm.me>
2025-02-09 10:02:07 -0800

    - Adds solution 2

--- modules/configuration.ts
+++
@@ -1,4 +0,0 @@
-import loadedConfig from "./../config/config.json"
-import loadedSecretConfig from "./../config/secret.json";
-
-export = { ...loadedConfig, ...loadedSecretConfig };
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,39 @@
-import { Interaction, Client, Collection, Events, GatewayIntentBits, MessageFlags } from 'discord.js';
-import * as fs from "fs";
-import * as path from "path";
-import config from './modules/configuration';
+import { Client, Collection, Events, MessageFlags, GatewayIntentBits } from 'discord.js';
 
-const client:any = new Client({ intents: [GatewayIntentBits.Guilds] });
+const fs = require('node:fs');
+const path = require('node:path');
+const { token } = require('../config.json');
+
+// interface DiscordClient<T> extends Client {
+// 	commands
+// }
+
+const client:any = new Client({ intents: [
+	GatewayIntentBits.Guilds,
+	GatewayIntentBits.GuildMessages,
+	GatewayIntentBits.MessageContent,
+	GatewayIntentBits.GuildMessageTyping,
+	GatewayIntentBits.GuildPresences,
+] });
 
 client.commands = new Collection();
 const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
+console.log('[DEBUG] folderPath: ', foldersPath);
+
 for (const folder of commandFolders) {
+	console.log('[DEBUG] folder: ', folder);
 	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter((file:any) => file.endsWith('.js'));
+	console.log('[DEBUG] commandsPath: ', commandsPath);
+	const commandFiles = fs.readdirSync(commandsPath).filter((file:string) => file.endsWith('.js'));
 	for (const file of commandFiles) {
 		const filePath = path.join(commandsPath, file);
 		const command = require(filePath);
+		console.log('[DEBUG] command: ', command);
 		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
+			console.log('[DEBUG] Setting command: ', command.data.name);
+			console.log('[DEBUG] Client set response: ', client.commands.set(command.data.name, command));
 		} else {
 			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
@@ -25,15 +42,14 @@ for (const folder of commandFolders) {
 
 client.once(Events.ClientReady, (readyClient:any) => {
 	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-  const channel: any = client.channels.cache.get('1239320263108329514');
+	const channel: any = client.channels.cache.get('1239320263108329514');
   if (channel) {
     channel.send('MonCasters bot ready!');
-    channel.send('Hello world!');
   }
 });
 
 client.on(Events.InteractionCreate, async (interaction:any) => {
-  console.log('Receivin an interaction: ', interaction);
+	console.log('[INFO] Receiving interaction: ', interaction.commandName);
 	if (!interaction.isChatInputCommand()) return;
 	const command = interaction.client.commands.get(interaction.commandName);
 
@@ -54,4 +70,4 @@ client.on(Events.InteractionCreate, async (interaction:any) => {
 	}
 });
 
-client.login(config.discordApi.token);
+client.login(token);
